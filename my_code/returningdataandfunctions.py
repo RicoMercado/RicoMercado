07de32b801d6a568a028624bc0d1ea6d659a5ab5
Rico <20240989@mywhitecliffe.com>
2024-09-05 14:02:29 +1200

    Create returningdataandfunctions.py

---
+++ my_code/returningdataandfunctions.py
@@ -0,0 +1,10 @@
+# ReturningDataandFunctions.py
+# Author: Rico Jhon Mercado
+# Date: September 2024
+def get_sum(num1, num2):
+    return num1 + num2
+
+print('\nSumming Two Numbers')
+num1 = int(input('\nEnter the first number: '))
+num2 = int(input('\nEnter the second number: '))
+print(f'\nThe sum of {num1} and {num2} is {get_sum(num1, num2)}')
